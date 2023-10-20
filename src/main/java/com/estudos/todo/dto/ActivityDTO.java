f659d4fdcd78aef747c71ef978b9269088f1e7bd
ViniBelo <vinialvesbelo@outlook.com>
2023-10-20 10:08:50 -0300

    Using record classes on DTOs

--- src/main/java/com/estudos/todo/dto/ActivityDTO.java
+++ src/main/java/com/estudos/todo/dto/ActivityDTO.java
@@ -2,18 +2,5 @@ package com.estudos.todo.dto;
 
 import com.estudos.todo.domain.entities.Activity;
 
-public class ActivityDTO {
-    private Activity activity;
-
-    public ActivityDTO(Activity activity) {
-        this.activity = activity;
-    }
-
-    public Activity getActivity() {
-        return activity;
-    }
-
-    public void setActivity(Activity activity) {
-        this.activity = activity;
-    }
+public record ActivityDTO (Activity activity){
 }
