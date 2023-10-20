3c65b9e4ad9364c1c8afd3330bf7e62a589786fb
ViniBelo <vinialvesbelo@outlook.com>
2023-10-20 10:55:24 -0300

    Enhancing Status enum

--- src/main/java/com/estudos/todo/domain/entities/enums/Status.java
+++ src/main/java/com/estudos/todo/domain/entities/enums/Status.java
@@ -1,9 +1,26 @@
 package com.estudos.todo.domain.entities.enums;
 
 public enum Status {
-    TO_DO(0),
-    IN_PROGRESS(1),
-    DONE(2);
+    TO_DO(1),
+    IN_PROGRESS(2),
+    DONE(3);
 
-    Status(int i) { }
+    private int status;
+
+    Status(int status) {
+        this.status = status;
+    }
+
+    public int getStatus() {
+        return status;
+    }
+
+    public static Status valueOf(int status) {
+        for (Status status1: Status.values()) {
+            if (status1.getStatus() == status) {
+                return status1;
+            }
+        }
+        throw new IllegalArgumentException("Invalid Status code!");
+    }
 }
